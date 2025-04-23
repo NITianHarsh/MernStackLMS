2aee8301d312718814c6284b3cca2fca5d71f37d
Gaurav Singh <gauravsinghchunar007@gmail.com>
2025-04-23 14:22:22 +0530

    student section navbar is added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import StudentViewCommonHeader from "./components/student-view/Header";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button className="bg-amber-400">Click me</Button>
-    </div>
+    // <div className="flex flex-col items-center justify-center min-h-svh">
+    //   <Button className="bg-amber-400">Click me</Button>
+    // </div>
+    <StudentViewCommonHeader/>
   );
 }
 
