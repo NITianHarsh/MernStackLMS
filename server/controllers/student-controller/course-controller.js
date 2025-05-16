1026c3e4c5ee501282279b59e09d972f10e71a40
Deepank Kujur <kujurdeepank@gmail.com>
2025-05-16 17:59:27 +0530

    few changes

--- server/controllers/student-controller/course-controller.js
+++ server/controllers/student-controller/course-controller.js
@@ -12,35 +12,35 @@ const getAllStudentViewCourses = async (req, res) => {
             sortBy = "price-lowtohigh",
         } = req.query;
 
-        let filters={};
-        if(category.length){
-            filters.category={$in : category.split(',')}
+        let filters = {};
+        if (category.length) {
+            filters.category = { $in: category.split(',') }
         }
-        if(level.length){
-            filters.level={$in : level.split(',')}
+        if (level.length) {
+            filters.level = { $in: level.split(',') }
         }
-        if(primaryLanguage.length){
-            filters.primaryLanguage={$in : primaryLanguage.split(',')}
+        if (primaryLanguage.length) {
+            filters.primaryLanguage = { $in: primaryLanguage.split(',') }
         }
 
-        let sortParam={};
+        let sortParam = {};
         switch (sortBy) {
             case 'price-lowtohigh':
-                sortParam.pricing=1
-            break;
+                sortParam.pricing = 1
+                break;
             case 'price-hightolow':
-                sortParam.pricing=-1
-            break;
+                sortParam.pricing = -1
+                break;
             case 'title-atoz':
-                sortParam.title=1
-            break;
+                sortParam.title = 1
+                break;
             case 'price-ztoa':
-                sortParam.title=-1
-            break;
-        
+                sortParam.title = -1
+                break;
+
             default:
-                sortParam.pricing=1
-            break;
+                sortParam.pricing = 1
+                break;
         }
         const courseList = await course.find(filters).sort(sortParam);
         res.status(200).json({
@@ -59,7 +59,7 @@ const getAllStudentViewCourses = async (req, res) => {
 
 const getStudentViewCourseDetails = async (req, res) => {
     try {
-        const { id} = req.params;
+        const { id } = req.params;
         const courseDetails = await course.findById(id);
 
         if (!courseDetails) {
@@ -73,7 +73,7 @@ const getStudentViewCourseDetails = async (req, res) => {
         res.status(200).json({
             success: true,
             data: courseDetails,
-            // coursePurchasedId:ifStudentAlreadyBoughtCurrentCourse?id:null,
+            // coursePurchasedId: ifStudentAlreadyBoughtCurrentCourse ? id : null,
         });
 
 
@@ -87,33 +87,61 @@ const getStudentViewCourseDetails = async (req, res) => {
     }
 
 }
+// const checkCoursePurchaseInfo = async (req, res) => {
+//     try {
+//         console.log('gaurav randi ')
+//         console.log(req.params)
+//         const { id, studentId } = req.params;
+//         console.log(id, 'id')
+//         console.log(studentId, 'stuuuuuuuuuuuuuu')
+//         const studentCourses = await StudentCourses.findOne({
+//             userId: studentId,
+//         });
+//         console.log(studentCourses, 'student coursesssssssssssssssssssssssssss')
+
+//         const ifStudentAlreadyBoughtCurrentCourse =
+//             studentCourses.courses.findIndex((item) => item.courseId.toString() === id.toString()) > -1;
+//         console.log(ifStudentAlreadyBoughtCurrentCourse)
+//         res.status(200).json({
+//             success: true,
+//             data: ifStudentAlreadyBoughtCurrentCourse,
+//         });
+//     } catch (e) {
+//         console.log(e);
+//         res.status(500).json({
+//             success: false,
+//             message: "Some error occured!",
+//         });
+//     }
+// };
 const checkCoursePurchaseInfo = async (req, res) => {
     try {
-        console.log('gaurav randi ')
-        console.log(req.params)
-      const { id, studentId } = req.params;
-      console.log(id,'id')
-      console.log(studentId,'stuuuuuuuuuuuuuu')
-      const studentCourses = await StudentCourses.findOne({
-        userId: studentId,
-      });
-      console.log(studentCourses,'student coursesssssssssssssssssssssssssss')
-  
-      const ifStudentAlreadyBoughtCurrentCourse =
-  studentCourses.courses.findIndex((item) => item.courseId.toString() === id.toString()) > -1;
-console.log(ifStudentAlreadyBoughtCurrentCourse)
-      res.status(200).json({
-        success: true,
-        data: ifStudentAlreadyBoughtCurrentCourse,
-      });
-    } catch (e) {
-      console.log(e);
-      res.status(500).json({
-        success: false,
-        message: "Some error occured!",
-      });
-    }
-  };
-  
+        const { id, studentId } = req.params;
 
-export { getAllStudentViewCourses, getStudentViewCourseDetails,checkCoursePurchaseInfo };
+        const studentCourses = await StudentCourses.findOne({ userId: studentId });
+
+        let ifStudentAlreadyBoughtCurrentCourse = false;
+
+        if (studentCourses && studentCourses.courses.length) {
+            ifStudentAlreadyBoughtCurrentCourse =
+                studentCourses.courses.findIndex(
+                    (item) => item.courseId.toString() === id.toString()
+                ) > -1;
+        }
+
+        res.status(200).json({
+            success: true,
+            data: ifStudentAlreadyBoughtCurrentCourse,
+        });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            success: false,
+            message: "Some error occurred!",
+        });
+    }
+};
+
+
+
+export { getAllStudentViewCourses, getStudentViewCourseDetails, checkCoursePurchaseInfo };
